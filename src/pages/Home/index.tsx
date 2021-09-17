fc8cb019d2274303c929dcc93bc79a1dba68d9fc
Beatriz Salles <beatrizsallesss@gmail.com>
2021-09-16 22:14:58 -0300

    refactor: Move home page into its own folder

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,11 @@
 import { FaPlus } from 'react-icons/fa';
 
-import { AppLayout } from '../layouts/AppLayout';
-import { Card } from '../components/Card';
+import { AppLayout } from '../../layouts/AppLayout';
+import { Card } from '../../components/Card';
 
 import { Container, Header, Title, NewUserButton, UserSection } from './styles';
 
-import users from '../utils/users.json'
+import users from '../../utils/users.json'
 
 export function Home() {
   return (
